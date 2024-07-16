325871be81c699dbebb532152143dcd752b064ae
AjithSimson <ajithsimson@gmail.com>
2024-07-16 10:54:19 +0530

    route structure

--- main.js
+++ main.js
@@ -8,6 +8,29 @@ app.get("/",(req,res)=>{
     res.json({msg:"Hello Students!"});
 });
 
+//crud functionality
+
+//R - reading
+app.get("/movies",()=>{
+
+});
+
+//creating movie
+app.post("/movies",()=>{
+
+});
+
+//u - updating movie
+
+app.put("/movies/:id",()=>{
+
+});
+
+//delete
+app.delete("/movie/:id", ()=>{
+
+});
+
 app.listen(PORT, ()=>{
     console.log(`The server is Running at http://localhost:${PORT}`);
 });
