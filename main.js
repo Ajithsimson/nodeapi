87cb7a612a97089922cedc20395b39e22e81178d
AjithSimson <ajithsimson@gmail.com>
2024-07-16 20:44:36 +0530

    connect to mongodb with env file

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import movieRoutes from './routes/movies.js'
+import connectDB from './lib/db.js'
 
 
 const app = express();
 const PORT = 6969;
+
+
+//Connect DB
+
+connectDB();
 
 app.get("/",(req,res)=>{
     res.json({msg:"Hello Students!!"});
--- routes/movies.js
+++ routes/movies.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { movieCreate, movieIndex, movieUpdate } from '../controller/movie.controller';
+import { movieCreate, movieIndex, movieUpdate } from '../controller/movie.controller.js';
 
 const router = express.Router();
 
